0e877620add5a90a79ca9bca2a0f7351f7a8cc91
Daniel Marohn <daniel.marohn@googlemail.com>
2013-02-10 16:52:20 +0100

    remove bogus code

--- abTest/context_processors.py
+++ abTest/context_processors.py
@@ -3,8 +3,3 @@ def ab(request):
     for test, result in request.abTest.items():
         experiments[test.name] = result.experiment.name
     return {'ab' : experiments}
-
-
-from abTest import goalReached
-def subscribedToNewsletter(request):
-    goalReached(request, 'subscribed newsletter')
